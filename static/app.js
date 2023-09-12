cfad0e8b60657fff30372ccf0ba0e4240d6daeec
Daniel Rhodes <danielrhodes47@gmail.com>
2023-09-12 08:52:07 -0500

    updated js and likes for a warble. still needs work.

--- static/app.js
+++ static/app.js
@@ -1,1 +1,29 @@
-alert('IT WORKS!')
+
+// app.js
+$(document).ready(function () {
+    // Event listener for the "Like" button click
+    $(".like-button").click(function () {
+        const warbleId = $(this).data("warble-id");
+
+        // Send an AJAX request to the server to like/unlike the warble
+        $.ajax({
+            url: `/like-warble/${warbleId}`, // Replace with your Flask route
+            method: "POST",
+            success: function (data) {
+                // Update the button or UI based on the response
+                if (data.success) {
+                    // The warble was liked/unliked successfully
+                    // Update the UI accordingly
+                    if (data.isLiked) {
+                        $(this).find("i").removeClass("bi-star").addClass("bi-star-fill");
+                    } else {
+                        $(this).find("i").removeClass("bi-star-fill").addClass("bi-star");
+                    }
+                }
+            },
+            error: function (error) {
+                console.error("Error:", error);
+            },
+        });
+    });
+});
