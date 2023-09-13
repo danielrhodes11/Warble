8fc78af9e0052ad35170533a5e17a93d80d1c9b9
Daniel Rhodes <danielrhodes47@gmail.com>
2023-09-12 20:27:31 -0500

    trying new ajax method...still not fixed

--- static/app.js
+++ static/app.js
@@ -1,4 +1,27 @@
-// implement ajax for like button
+$('.like-button').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation(); // Prevent the click event from propagating to the anchor link
 
+    const button = $(this);
+    const messageID = button.data('message-id');
+    const isLiked = button.data('liked');
+
+    // Send an AJAX request to like/unlike the message
+    $.ajax({
+        method: 'POST',
+        url: `/users/add_like/${messageID}`,
+        data: { liked: !isLiked },
+        success: function (response) {
+            // Update the button text and like status
+            if (isLiked) {
+                button.data('liked', false);
+                button.html('<i class="far fa-heart"></i>');
+            } else {
+                button.data('liked', true);
+                button.html('<i class="fas fa-heart"></i>');
+            }
+        }
+    });
+});
 
 
