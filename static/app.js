36e69f444e4e358e4d9b7e13e5522d24847204d3
Daniel Rhodes <danielrhodes47@gmail.com>
2023-09-12 19:42:16 -0500

    removed logic for ajax, trying new method.

--- static/app.js
+++ static/app.js
@@ -1,29 +1,4 @@
+// implement ajax for like button
 
-// app.js
-$(document).ready(function () {
-    // Event listener for the "Like" button click
-    $(".like-button").click(function () {
-        const warbleId = $(this).data("warble-id");
 
-        // Send an AJAX request to the server to like/unlike the warble
-        $.ajax({
-            url: `/like-warble/${warbleId}`, // Replace with your Flask route
-            method: "POST",
-            success: function (data) {
-                // Update the button or UI based on the response
-                if (data.success) {
-                    // The warble was liked/unliked successfully
-                    // Update the UI accordingly
-                    if (data.isLiked) {
-                        $(this).find("i").removeClass("bi-star").addClass("bi-star-fill");
-                    } else {
-                        $(this).find("i").removeClass("bi-star-fill").addClass("bi-star");
-                    }
-                }
-            },
-            error: function (error) {
-                console.error("Error:", error);
-            },
-        });
-    });
-});
+
