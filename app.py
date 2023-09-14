033383eeec4f495c871582a25e6659a0bc8843ed
Daniel Rhodes <danielrhodes47@gmail.com>
2023-09-14 10:35:06 -0500

    updated

--- app.py
+++ app.py
@@ -434,6 +434,13 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    """500 INTERNAL SERVER ERROR page."""
+
+    return render_template('500.html'), 500
+
+
 ##############################################################################
 # Turn off all caching in Flask
 #   (useful for dev; in production, this kind of stuff is typically
